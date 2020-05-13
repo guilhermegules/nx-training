a28e543b825bb16f0b127882df8a12f00880818b
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-05-13 12:00:11 -0300

    (feat): adding api service

---
+++ todos-org/apps/todos/src/app/services/api.service.ts
@@ -0,0 +1,19 @@
+import { Todo } from './../interfaces/todo';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  fetchApi(): Observable<Todo[]> {
+    return this.http.get<Todo[]>('/api/todos');
+  }
+
+  addTodo(): Observable<Todo> {
+    return this.http.post<Todo>('/api/addTodo', {});
+  }
+}
