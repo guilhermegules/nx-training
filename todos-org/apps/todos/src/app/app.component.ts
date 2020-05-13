5f37b0eb817d29a79a9b0b05c1d6ac047345b423
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-05-13 11:16:05 -0300

    creating todos organization

---
+++ todos-org/apps/todos/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todos-org-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'todos';
+}
